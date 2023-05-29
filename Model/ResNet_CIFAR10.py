27b60c2706814d113b2914d29af31547592c30fb
nhn1030 <shgusska12@gmail.com>
2023-05-29 23:42:30 +0900

    module

--- Model/ResNet_CIFAR10.py
+++ Model/ResNet_CIFAR10.py
@@ -6,10 +6,10 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_planes)
-        self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_planes)
         self.shortcut = nn.Sequential()
-        if stride != 1:
+        if stride != 1 or in_planes != out_planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_planes)
--- train.py
+++ train.py
@@ -1,118 +1,104 @@
-import sys
-import os
-model_dir = os.path.abspath('/Users/nohyeonnam/Documents/exercise/Model')
-sys.path.append(model_dir)
-
-
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-from torchvision.datasets import CIFAR10
-from torchvision.transforms import Compose, RandomCrop, RandomHorizontalFlip, ToTensor
-
-from torch.utils.data import DataLoader
+import os
+import time
 from Model.ResNet_CIFAR10 import ResNet18
-
-
-
-def get_train_loader(batch_size, num_workers):
-    transform_train = Compose([
-        RandomCrop(32, padding=4),
-        RandomHorizontalFlip(),
-        ToTensor(),
-    ])
-
-    train_dataset = CIFAR10(
-        root='./datasets', train=True, download=True, transform=transform_train
-    )
-
-    train_loader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
-    )
-
-    return train_loader
-
-
-def get_test_loader(batch_size, num_workers):
-    transform_test = Compose([
-        ToTensor(),
-    ])
-
-    test_dataset = CIFAR10(
-        root='./datasets', train=False, download=True, transform=transform_test
-    )
-
-    test_loader = DataLoader(
-        test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers
-    )
-
-    return test_loader
-
-
-def train(net, train_loader, criterion, optimizer, device):
-    print('\n[ Train ]')
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-
-        optimizer.step()
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-
-        total += targets.size(0)
-        current_correct = predicted.eq(targets).sum().item()
-        correct += current_correct
-
-        if batch_idx % 100 == 0:
-            print('\nCurrent batch:', str(batch_idx))
-            print('Current batch average train accuracy:', current_correct / targets.size(0))
-            print('Current batch average train loss:', loss.item() / targets.size(0))
-
-    print('\nTotal average train accuracy:', correct / total)
-    print('Total average train loss:', train_loss / total)
-
-
-def test(net, test_loader, criterion, device, file_name):
-    print('\n[ Test ]')
-    net.eval()
-    loss = 0
-    correct = 0
-    total = 0
-
-    for batch_idx, (inputs, targets) in enumerate(test_loader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        total += targets.size(0)
-
-        outputs = net(inputs)
-        loss += criterion(outputs, targets).item()
-
-        _, predicted = outputs.max(1)
-        correct += predicted.eq(targets).sum().item()
-
-    print('\nTotal average test accuracy:', correct / total)
-    print('Total average test loss:', loss / total)
-
-    state = {
-        'net': net.state_dict()
-    }
-    if not os.path.isdir('checkpoint'):
-        os.mkdir('checkpoint')
-    torch.save(state, './checkpoint/' + file_name)
-    print('Model saved!')
-
+from datasets.data_CIFAR10 import get_train_loader as train_loader
+from datasets.data_CIFAR10 import get_test_loader as test_loader
 
 if __name__ == '__main__':
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    train_loader = train_loader(batch_size=128, num_workers=2)
+    test_loader = test_loader(batch_size=128, num_workers=2)
 
-    net = ResNet18().to(device)
+
+    device = 'cpu'
+
+    net = ResNet18()
+    net = net.to(device)
 
     learning_rate = 0.1
+    file_name = 'resnet18_cifar10.pth'
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0002)
+
+
+    def train(epoch):
+        print('\n[ Train epoch: %d ]' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
+
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+
+            optimizer.step()
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+
+            total += targets.size(0)
+            current_correct = predicted.eq(targets).sum().item()
+            correct += current_correct
+            
+            if batch_idx % 100 == 0:
+                print('\nCurrent batch:', str(batch_idx))
+                print('Current batch average train accuracy:', current_correct / targets.size(0))
+                print('Current batch average train loss:', loss.item() / targets.size(0))
+
+        print('\nTotal average train accuarcy:', correct / total)
+        print('Total average train loss:', train_loss / total)
+
+
+    def test(epoch):
+        print('\n[ Test epoch: %d ]' % epoch)
+        net.eval()
+        loss = 0
+        correct = 0
+        total = 0
+
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            total += targets.size(0)
+
+            outputs = net(inputs)
+            loss += criterion(outputs, targets).item()
+
+            _, predicted = outputs.max(1)
+            correct += predicted.eq(targets).sum().item()
+
+        print('\nTotal average test accuarcy:', correct / total)
+        print('Total average test loss:', loss / total)
+
+        state = {
+            'net': net.state_dict()
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/' + file_name)
+        print('Model Saved!')
+
+
+    def adjust_learning_rate(optimizer, epoch):
+        lr = learning_rate
+        if epoch >= 50:
+            lr /= 10
+        if epoch >= 100:
+            lr /= 10
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+    start_time = time.time()
+
+    for epoch in range(0, 150):
+        adjust_learning_rate(optimizer, epoch)
+        train(epoch)
+        test(epoch)
+        print('\nTime elapsed:', time.time() - start_time)
