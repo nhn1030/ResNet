e41cbb279493fb40390996d11fb7a8f78e8b4a93
nhn1030 <shgusska12@gmail.com>
2023-05-29 19:12:58 +0900

    first commit

--- datasets/data_cifar10.py
+++ datasets/data_cifar10.py
@@ -1,14 +1,19 @@
+import torch
 import torchvision
 import torchvision.transforms as transforms
 
-transform_train = transforms.compose([
+transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
 ])
 
-transform_test = transforms.compose([
+transform_test = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-train_dataset = torchvision.train_dataset
+train_dataset = torchvision.datasets.CIFAR10(root= './datasets', train=True, download= True, transform=transform_train)
+test_dataset = torchvision.datasets.CIFAR10(root= './datasets', train=True, download= True, transform=transform_test)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size= 100, shuffle=False, num_workers=2)
