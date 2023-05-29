92f0a1ab5bc954a2007023c176159d708bdc701c
노현남 <shgusska12@gmail.com>
2023-05-29 16:04:10 +0000

    test

--- train.py
+++ train.py
@@ -12,8 +12,7 @@ if __name__ == '__main__':
     train_loader = train_loader(batch_size=128, num_workers=2)
     test_loader = test_loader(batch_size=128, num_workers=2)
 
-
-    device = 'cpu'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     net = ResNet18()
     net = net.to(device)
@@ -23,7 +22,6 @@ if __name__ == '__main__':
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0002)
-
 
     def train(epoch):
         print('\n[ Train epoch: %d ]' % epoch)
@@ -53,9 +51,8 @@ if __name__ == '__main__':
                 print('Current batch average train accuracy:', current_correct / targets.size(0))
                 print('Current batch average train loss:', loss.item() / targets.size(0))
 
-        print('\nTotal average train accuarcy:', correct / total)
-        print('Total average train loss:', train_loss / total)
-
+        print('\nTotal average train accuracy:', correct / total)
+        return train_loss / total
 
     def test(epoch):
         print('\n[ Test epoch: %d ]' % epoch)
@@ -74,31 +71,40 @@ if __name__ == '__main__':
             _, predicted = outputs.max(1)
             correct += predicted.eq(targets).sum().item()
 
-        print('\nTotal average test accuarcy:', correct / total)
-        print('Total average test loss:', loss / total)
+        print('\nTotal average test accuracy:', correct / total)
+        return loss / total
 
-        state = {
-            'net': net.state_dict()
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/' + file_name)
-        print('Model Saved!')
-
-
-    def adjust_learning_rate(optimizer, epoch):
-        lr = learning_rate
-        if epoch >= 50:
-            lr /= 10
-        if epoch >= 100:
-            lr /= 10
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
+    early_stopping_epochs = 10  # 조기 종료를 위한 에포크 수
+    best_loss = float('inf')  # 최적의 손실값 초기화
+    early_stopping_counter = 0  # 조기 종료 카운터 초기화
+    best_model_state = None  # 최적 모델의 상태
 
     start_time = time.time()
 
     for epoch in range(0, 150):
-        adjust_learning_rate(optimizer, epoch)
-        train(epoch)
-        test(epoch)
+        train_loss = train(epoch)
+        test_loss = test(epoch)
         print('\nTime elapsed:', time.time() - start_time)
+
+        # 최적 성능 갱신 여부 확인
+        if test_loss < best_loss:
+            best_loss = test_loss  # 최적 성능 갱신
+            early_stopping_counter = 0  # 개선되지 않은 에포크 카운터 초기화
+            best_model_state = net.state_dict()  # 최적 모델의 상태 저장
+        else:
+            early_stopping_counter += 1  # 개선되지 않은 에포크 카운터 증가
+
+        # 개선되지 않은 에포크 횟수가 일정 값 이상인 경우 조기 종료
+        if early_stopping_counter >= early_stopping_epochs:
+            print("조기 종료!")
+            break
+
+    # 최적 모델 저장
+    if best_model_state is not None:
+        state = {
+            'net': best_model_state
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/' + file_name)
+        print('Best Model Saved!')
